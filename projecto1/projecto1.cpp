9c362713ed675d566f2d29f797d6cbc23a9ac43d
Alejandro <alejandromangil@campus.monlau.com>
2025-04-25 13:24:03 +0200

    ejemplo de clase

---
+++ projecto1/projecto1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 18;
+    string name = "Alejandro Manotas";
+    cout << "Hello World! " + name + endl;
+    cout << "Mi edad es " + age;
+
+}
