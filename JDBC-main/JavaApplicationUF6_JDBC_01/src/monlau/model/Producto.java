e35bc7633d0d2d4ebc84cd377a1e569f39599432
gab1maruuu <amanotas800@gmail.com>
2025-05-13 10:03:43 +0200

    Add files via upload

---
+++ JDBC-main/JavaApplicationUF6_JDBC_01/src/monlau/model/Producto.java
@@ -0,0 +1,49 @@
+package monlau.model;
+
+public class Producto {
+    private Integer id;
+    private String nombre;
+    private Double precio;
+
+    public Producto() {
+    }
+
+    public Producto(Integer id, Double precio, String nombre) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
